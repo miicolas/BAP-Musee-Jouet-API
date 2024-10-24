bb2b163caeccf4f78ee9c9a3eb7d1e5f8ed3d586
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-10-24 11:58:18 +0200

    connection bw 2 pg

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import { Server as socketIO } from 'socket.io';
-import http from 'http';
+import express from "express";
+import cors from "cors";
+import { Server as socketIO } from "socket.io";
+import http from "http";
 
 const app = express();
 const PORT = 4000;
@@ -12,20 +12,29 @@ const server = http.createServer(app);
 
 const io = new socketIO(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("question", (questionId) => {
+    console.log("Received question ID:", questionId);
+    io.emit("question", questionId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   res.json({
-    message: 'Hello world',
+    message: "Hello world",
   });
 });
 
